0febd6509a5a282bebc1b181cabaa27f0d156ae4
Jorge Barragan <developbarragan@gmail.com>
2024-08-18 15:02:07 -0600

    Feat Titulos y orientacion de botonesProyecto iniciado

--- S4.Dominio.Comunes/obj/Debug/net7.0/S4.Dominio.Comunes.AssemblyInfo.cs
+++ S4.Dominio.Comunes/obj/Debug/net7.0/S4.Dominio.Comunes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S4.Dominio.Comunes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91aeda6ee5842bc6dda3c6520042597d52a4a2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("S4.Dominio.Comunes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S4.Dominio.Comunes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
